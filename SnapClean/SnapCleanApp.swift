5396b58cf296279e46fdf3903debbfe8f2b55436
Tung Nguyen <nxtungbkhn@gmail.com>
2024-02-26 23:52:08 +0700

    Initial Commit

---
+++ SnapClean/SnapCleanApp.swift
@@ -0,0 +1,20 @@
+//
+//  SnapCleanApp.swift
+//  SnapClean
+//
+//  Created by Tung Nguyen on 26/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct SnapCleanApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
